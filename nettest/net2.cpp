857699f65dd831771fb78cb44207e4d97ea3a79a
ethan <18201521626@139.com>
2022-10-28 16:59:56 +0800

    Update net2.cpp

--- nettest/net2.cpp
+++ nettest/net2.cpp
@@ -1,9 +1,5 @@
-
-#include "../include/net2.h"
-
-#include <memory>
-
 #include "../include/optimize.h"
+#include "../include/net2.h"
 
 using namespace std;
 int main() {
@@ -12,15 +8,10 @@ int main() {
     layer_seq FCN;
     FCN.add(make_shared<linear>(2, 20));
     FCN.add(make_shared<hardswish>());
-    /* FCN.add(make_shared<batchnorm_adam>(20)); */
-
     FCN.add(make_shared<linear>(20, 20));
     FCN.add(make_shared<hardswish>());
-    /* FCN.add(make_shared<batchnorm>(20)); */
-
     FCN.add(make_shared<linear>(20, 20));
     FCN.add(make_shared<hardswish>());
-    /* FCN.add(make_shared<batchnorm_adam>(20)); */
     FCN.add(make_shared<linear>(20, 20));
     FCN.add(make_shared<hardswish>());
     FCN.add(make_shared<linear>(20, 20));
@@ -66,12 +57,10 @@ int main() {
     batch data;
     for (int i = 1; i <= 10000; i++) {
         VectorXd in(2), out(2);
-        /* double x = ri(0, 1), y = ri(0, 1); */
         double x = rd(-1, 1), y = rd(-1, 1);
         in << x, y;
         out << (x * x + y * y <= 0.5 && x * x + y * y >= 0.3 ? 1 : 0),
             (x * x + y * y <= 0.5 && x * x + y * y >= 0.3 ? 0 : 1);
-        /* out << (int(x) ^ int(y)); */
         data.first.push_back(in);
         data.second.push_back(out);
     }
@@ -79,9 +68,6 @@ int main() {
     /* sgd( */
     /* sliced_data, FCN, 64, 10000, [](int x) { return 1. / 64. / (1. + x * 0.005); }, chk_k); */
     adam(sliced_data, FCN, 128, 10000, crossentropy_k);
-    // 2.32 2.31 2.35
-    // 2.23 2.29 2.39
-
     FCN.set_train_mode(0);
     while (1) {
         double x, y;
